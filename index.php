ae79bd5e31c3c3467a4bc0e5125258cec40847ed
k-dimitrijevs <kristians.dimitrijevs@gmail.com>
2021-10-06 15:04:05 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/todo-list', 'TodoController@index');
+    $r->addRoute('POST', '/todo-list/add', 'TodoController@add');
+    $r->addRoute('GET', '/todo-list/delete', 'TodoController@delete');
+});
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        // ... call $handler with $vars
+
+        [$controller, $method] = explode('@', $handler);
+        $controller = 'App\Controllers\\' . $controller;
+        $controller = new $controller();
+        $controller->$method();
+        break;
+}
